a492b2d60147a9193394268b8a7be8b1cbca996b
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2024-01-24 10:51:13 -0300

    aplicação da font e criação dos estilos utilizados

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import react, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className='calculator'>
+                
+            </div>
+        )
+    }
+}
